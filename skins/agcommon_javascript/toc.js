f46f23502c1f1c89762760ffd1621c2acdbb4bc7
tsimkins <trs22@psu.edu>
2015-01-09 15:57:47 -0500

    Updated to latest version of toc.js, and added customizations.

--- skins/agcommon_javascript/toc.js
+++ skins/agcommon_javascript/toc.js
@@ -1,35 +1,47 @@
-(function($) { $(function() {
-    var dest = $('.toc dd.portletItem');
-    var content = getContentArea();
-    if (!content || !dest.length) return;
-    
-    $('#content').addClass('hasToc');
-    
+/* Creates table of contents for pages for h[1234] */
+
+jQuery(function($) {
+    var dest, content, location, stack, oltoc, numdigits, wlh, target,
+        targetOffset;
+
+    dest = $('.toc dd.portletItem');
+    content = $('#region-content,#content');
+
+    if (!content || !dest.length) {return;}
+
+    $(content).addClass('hasToc');
+
     dest.empty();
 
-    var location = window.location.href;
-    if (window.location.hash)
+    location = window.location.href;
+    if (window.location.hash) {
         location = location.substring(0, location.lastIndexOf(window.location.hash));
-
-    var stack = [];
+    }
+    stack = [];
     // Get headers in document order
     $(content).find('*').not('.comment > h3').not('.documentActions > h2').not('#addthis > h2').not('.publication > h2').filter(function() { return /^h[1234]$/.test(this.tagName.toLowerCase()) })
         .not('.documentFirstHeading').not('.portletHeader').each(function(i) {
-        var level = this.nodeName.charAt(1);
+
+        var level, ol, li;
+
+        level = this.nodeName.charAt(1);
+
         // size the stack to the current level
         while (stack.length < level) {
-            var ol = $('<ol>');
+            ol = $('<ol>');
             if (stack.length) {
-                var li = $(stack[stack.length - 1]).children('li:last');
-                if (!li.length)
+                li = $(stack[stack.length - 1]).children('li:last');
+                if (!li.length) {
                     // create a blank li for cases where, e.g., we have a subheading before any headings
                     li = $('<li>').appendTo($(stack[stack.length - 1]));
+                }
                 li.append(ol);
             }
             stack.push(ol);
         }
-        while (stack.length > level) stack.pop();
-        
+
+        while (stack.length > level) {stack.pop();}
+
         $(this).before($('<a name="section-' + i + '" />'));
         $('<li>').append(
             $('<a />').attr('href', location + '#section-' + i)
@@ -38,7 +50,6 @@
     });
 
     if (stack.length) {
-        $('.toc').show();
         var oltoc = $(stack[0]);
         // first level is a level with at least two entries #11160
         var i = 1;
@@ -46,20 +57,26 @@
             oltoc = $(stack[i]);
             i += 1;
         }
+
+        if (i <= stack.length) {
+            $('.toc').show();
+        }
+
         numdigits = oltoc.children().length.toString().length;
         //Use a clever class name to add margin that's MUCH easier to customize
         oltoc.addClass("TOC"+numdigits+"Digit");
         dest.append(oltoc);
 
         //scroll to element now.
-        var wlh = window.location.hash;
+        wlh = window.location.hash;
         if (wlh) {
-            var target = $(wlh);
+            target = $(wlh);
             target = target.length && target
-                || $('[name=' + wlh.slice(1) +']');
-            var targetOffset = target.offset();
-            if (targetOffset)
+                || $('[name="' + wlh.slice(1) +'"]');
+            targetOffset = target.offset();
+            if (targetOffset) {
                 $('html,body').animate({scrollTop: targetOffset.top}, 0);
+            }
         }
     }
-}); })(jQuery);
+});
