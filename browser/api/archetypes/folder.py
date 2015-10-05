71fe62202c212ed26b9b2ca8afc080fd581031e4
tsimkins <trs22@psu.edu>
2015-10-05 16:31:08 -0400

    Check for presence of default page that's a collection, and if that's the case, return that item's data

--- browser/api/archetypes/folder.py
+++ browser/api/archetypes/folder.py
@@ -8,6 +8,14 @@ class FolderView(BaseView):
     def getData(self, recursive=True):
         data = self.getBaseData()
 
+        # Check for presence of default page that's a collection, and if that's
+        # the case, return that item's data
+        context = self.context
+        default_page_id = context.getDefaultPage()
+        
+        if default_page_id and default_page_id in context.objectIds():
+            return context[default_page_id].restrictedTraverse('@@api-json').getData(recursive=recursive)
+
         if recursive:
             contents = self.getContents()
             
