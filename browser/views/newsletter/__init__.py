40b39089793006adc037369f12991bff3f56f3ee
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2017-09-19 09:22:33 -0400

    * Added a public_url call that pulls from the registry key 'agsci.newsletter.domain' rather than converting https:// to http://.
     * CSS fix for h1 line height

--- browser/views/newsletter/__init__.py
+++ browser/views/newsletter/__init__.py
@@ -11,7 +11,7 @@ from plone.registry.record import Record
 from plone.registry.registry import Registry
 from Products.CMFCore import permissions
 from Products.CMFCore.utils import getToolByName
-from urlparse import urljoin
+from urlparse import urljoin, urlparse, urlunparse
 from zope.component import getUtility, getMultiAdapter
 from zope.interface import implements, Interface
 from zope.security import checkPermission
@@ -38,7 +38,7 @@ class IEventInvitationView(Interface):
 class NewsletterView(AgCommonUtilities, LeadImageViewlet):
 
     implements(INewsletterView)
-    
+
     def __init__(self, context, request):
         self.context = context
         self.request = request
@@ -68,8 +68,8 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
             field = context.getField('image')
 
         if not titlef:
-            titlef = context.getField('imageCaption')        
-        
+            titlef = context.getField('imageCaption')
+
         if titlef is not None:
             title = titlef.get(context)
         else:
@@ -87,7 +87,7 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
     @property
     def portal(self):
         return getToolByName(self.context, 'portal_url').getPortalObject()
-        
+
     def isEnabled(self, item):
 
         enabled_items = self.getEnabledUID()
@@ -100,7 +100,7 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
     def isSpotlight(self, item):
 
         return item.UID in self.getSpotlightUID()
-        
+
     def showItem(self, item, item_type='enabled'):
         if item_type=='spotlight':
             return self.isSpotlight(item)
@@ -112,7 +112,7 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
     @property
     def showSummary(self):
         show_summary = self.getConfig('show_summary')
-        
+
         if show_summary == 'yes':
             return True
         elif show_summary == 'no':
@@ -132,11 +132,11 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
             folderContents = self.context.queryCatalog(batch=True, **contentFilter)
 
             if order_by_id or order_by_title:
-                folderContents = self.reorderTopicContents(folderContents, order_by_id=order_by_id, order_by_title=order_by_title) 
+                folderContents = self.reorderTopicContents(folderContents, order_by_id=order_by_id, order_by_title=order_by_title)
         else:
             folderContents = self.context.getFolderContents(contentFilter, batch=True)
 
-        return folderContents   
+        return folderContents
 
     @property
     def registry(self):
@@ -152,7 +152,7 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
             return (sc.field, sort_dir)
         else:
             return ('effective', 'descending')
-        
+
     def getConfig(self,  key=''):
         try:
             value = self.registry.records[self.getRegistryKey()].value.get(key)
@@ -162,13 +162,10 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
                     value = [value]
 
                 return value
-                
-                #contents_uid = set([x.UID for x in self.folderContents()])
-    
-                #return list(contents_uid.intersection(set(value)))
+
             else:
                 return value
-            
+
         except (KeyError, AttributeError):
             return []
 
@@ -184,41 +181,41 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
     def getEnabledItems(self):
         non_spotlight_uids = list(set(self.getEnabledUID()) - set(self.getSpotlightUID()))
         (sort_on, sort_order) = self.getSortCriterion()
-        results = self.portal_catalog.searchResults({'UID' : non_spotlight_uids, 'sort_on' : sort_on, 'sort_order' : sort_order})    
+        results = self.portal_catalog.searchResults({'UID' : non_spotlight_uids, 'sort_on' : sort_on, 'sort_order' : sort_order})
 
         order_by_title = getattr(self.context, 'order_by_title', None)
         order_by_id = getattr(self.context, 'order_by_id', None)
 
         if order_by_id or order_by_title:
-            results = self.reorderTopicContents(results, order_by_id=order_by_id, order_by_title=order_by_title) 
+            results = self.reorderTopicContents(results, order_by_id=order_by_id, order_by_title=order_by_title)
 
         return results
 
 
     def getSpotlightItems(self):
         (sort_on, sort_order) = self.getSortCriterion()
-        return self.portal_catalog.searchResults({'UID' : self.getSpotlightUID(), 'sort_on' : sort_on, 'sort_order' : sort_order})    
+        return self.portal_catalog.searchResults({'UID' : self.getSpotlightUID(), 'sort_on' : sort_on, 'sort_order' : sort_order})
 
     def setConfig(self, enabled=[], spotlight=[], show_summary=[]):
         # Make all spotlight articles enabled
         enabled = list(set(enabled).union(set(spotlight)))
-    
+
         self.registry.records[self.getRegistryKey()] = Record(field.Dict(title=u"Item UIDs"), {'enabled' : enabled, 'spotlight' : spotlight, 'show_summary' : show_summary})
-        
+
     def getRegistryKey(self):
         return 'agsci.newsletter.uid_%s' % self.context.UID()
 
     def getViewOnline(self):
-    
+
         more_url = getattr(self.context, 'more_url', None)
 
         if more_url:
             return more_url
-    
+
         if getattr(self.context, 'hide_view_online', False):
-        
+
             return None
-    
+
         parent = self.context.getParentNode()
 
         if parent.portal_type == 'Blog':
@@ -228,7 +225,7 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
 
 
     def getHTML(self, item):
-    
+
         text = item.getObject().newsletter_full_view_item()
         soup = BeautifulSoup(text)
 
@@ -237,7 +234,7 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
             t.extract()
 
         # Fix Images
-        
+
         for i in soup.findAll('img'):
             try:
                 src = i['src']
@@ -247,9 +244,9 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
             if not src.startswith('http') :
                 i['src'] = urljoin(item.getURL(), src)
 
-        
+
         # Fix URLs
-        
+
         for a in soup.findAll('a'):
             try:
                 href = a['href']
@@ -273,19 +270,19 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
 
             elif '@' not in contents and href.startswith('mailto'):
                 a.append(BeautifulSoup(" ( <strong>%s</strong> ) " % a['href'].replace('mailto:', '')))
-   
+
             else:
                 a['class'] = 'standalone'
 
         html = repr(soup)
-        html = html.replace('The external news article is:', 'Full article:') # If there's a news item article link        
+        html = html.replace('The external news article is:', 'Full article:') # If there's a news item article link
 
         return html
 
 
     @property
     def newsletter(self):
-       
+
         if self.context.portal_type in ['Newsletter'] or \
            (self.context.portal_type in ['Topic'] and self.context.getLayout() == 'newsletter_view'):
             return self.context
@@ -293,7 +290,7 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
         elif self.context.isPrincipiaFolderish:
 
             newsletters = self.context.listFolderContents({'portal_type' : 'Newsletter'})
-    
+
             if not newsletters:
                 return None
             elif len(newsletters) == 1:
@@ -307,28 +304,28 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
                     return newsletters[0]
 
         return None
-       
+
 
 
     @property
     def newsletter_title(self):
 
         newsletter = self.newsletter
-        
+
         if newsletter:
             newsletter_title = getattr(newsletter, 'newsletter_title', None)
 
             if newsletter_title:
                 return newsletter_title
-            
+
             return self.newsletter.Title()
-        
+
         else:
             return self.context.Title()
 
     # If we have a @lists.psu.edu listserv, return the listserv name
 
-    @property    
+    @property
     def listserv(self):
         domain = "lists.psu.edu".upper()
         newsletter = self.newsletter
@@ -342,21 +339,21 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
     def listserv_contact_email(self):
 
         listserv = self.listserv
-        
+
         if listserv:
             subject = urllib.quote('Question about %s' % listserv)
             return '%s-request@lists.psu.edu?Subject=%s' % (listserv, subject)
         else:
-            return None 
-        
+            return None
+
     def listserv_unsubscribe_email(self):
-        
+
         return self.listserv_subscribe_email(action="unsubscribe")
 
     def listserv_subscribe_email(self, action="subscribe"):
-        
+
         listserv = self.listserv
-        
+
         if listserv:
             return '%s-%s-request@lists.psu.edu' % (listserv, action)
         else:
@@ -366,7 +363,7 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
     def listserv_url(self):
 
         listserv = self.listserv
-        
+
         if listserv:
             return 'http://lists.psu.edu/cgi-bin/wa?SUBED1=%s&A=1' % listserv
         else:
@@ -383,7 +380,34 @@ class NewsletterView(AgCommonUtilities, LeadImageViewlet):
         else:
             return ''
 
-        
+    @property
+    def public_url(self):
+
+        # Registry domain value
+        domain = self.registry.get('agsci.newsletter.domain', None)
+
+        # Calculated URL
+        url = self.context.absolute_url().replace('https://', 'http://')
+
+        if domain:
+            # Parse URLs so we can interpolate
+            parsed_domain = urlparse(domain)
+            parsed_url = urlparse(url)
+
+            # Return interpolated value
+            return urlunparse(
+                [
+                    parsed_domain.scheme,
+                    parsed_domain.netloc,
+                    parsed_url.path,
+                    '',
+                    '',
+                    ''
+                ]
+            )
+
+        return url
+
 class NewsletterModify(NewsletterView):
 
     security = ClassSecurityInfo()
@@ -393,7 +417,7 @@ class NewsletterModify(NewsletterView):
         enabled_items = self.request.form.get('enabled_items', [])
         spotlight_items = self.request.form.get('spotlight_items', [])
         show_summary = self.request.form.get('show_summary', 'auto')
-        
+
         if not isinstance(enabled_items, list):
             enabled_items = [enabled_items]
 
@@ -403,49 +427,49 @@ class NewsletterModify(NewsletterView):
         self.setConfig(enabled=enabled_items, spotlight=spotlight_items, show_summary=show_summary)
 
         self.request.response.redirect(self.context.absolute_url())
-        
+
 class NewsletterEmail(NewsletterView):
 
     implements(INewsletterView)
 
     def render(self):
         return self.index()
-        
+
     def __call__(self):
-    
+
         html = self.render()
         html = html.replace('&nbsp;', ' ')
-    
+
         soup = BeautifulSoup(html)
-        
+
         for img in soup.findAll('img', {'class' : 'leadimage'}):
             img['hspace'] = 8
             img['vspace'] = 8
 
 
         if self.anonymous:
-    
+
             for a in soup.findAll('a'):
                 if 'no_utm' in a.get('class', ''):
                     continue
                 klass = [x.replace('utm_', '') for x in a.get('class', '').split() if x.startswith('utm_')]
                 utm_content = None
-                
+
                 if klass:
                     utm_content = klass[0]
-                    
-                utm  = self.getUTM(source='newsletter', medium='email', 
+
+                utm  = self.getUTM(source='newsletter', medium='email',
                                    campaign=self.newsletter_title, content=utm_content);
 
                 if '?' in a['href']:
-                    a['href'] = '%s&%s' % (a['href'], utm) 
+                    a['href'] = '%s&%s' % (a['href'], utm)
                 else:
-                    a['href'] = '%s?%s' % (a['href'], utm)            
+                    a['href'] = '%s?%s' % (a['href'], utm)
 
         html = premailer.transform(unicode(repr(soup), 'utf-8'))
 
         tags = ['dl', 'dt', 'dd']
-        
+
         for tag in tags:
             html = html.replace("<%s" % tag, "<div")
             html = html.replace("</%s" % tag, "</div")
